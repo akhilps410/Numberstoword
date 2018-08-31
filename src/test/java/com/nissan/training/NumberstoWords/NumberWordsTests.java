a3725f0f1acec05f7e325786e315afd831ca6a54
akhilps410 <akhilps410@gmail.com>
2018-08-31 23:50:21 +0530

    Second test modification

--- src/test/java/com/nissan/training/NumberstoWords/NumberWordsTests.java
+++ src/test/java/com/nissan/training/NumberstoWords/NumberWordsTests.java
@@ -12,27 +12,27 @@ public class NumberWordsTests {
 	
 
 	@Test
-	public void numberOutOfRangeReturnsError() {
+	public void testnumberOutOfRangeReturnsError() {
 		Assert.assertEquals( "Error", "Number out of range", numberWords.toWords( -1 ) ) ;
 	}
 	@Test
-	public void numberinbtw1to20() {
+	public void testnumberinbtw1to20() {
 		Assert.assertEquals("Five", numberWords.toWords(5) ) ;
 	}
 	@Test
-	public void numberinbtw21to100() {
+	public void testnumberinbtw21to100() {
 		Assert.assertEquals("Fifty Four", numberWords.toWords(54) ) ;
 	}
 	@Test
-	public void numberinbtw101to1000() {
+	public void testnumberinbtw101to1000() {
 		Assert.assertEquals("Eight Hundred Fifty Four", numberWords.toWords(854) ) ;
 	}
 	@Test
-	public void numberinbtw1001to100000() {
+	public void testnumberinbtw1001to100000() {
 		Assert.assertEquals("Thirty Five Thousand Eight Hundred Fifty Four", numberWords.toWords(35854) ) ;
 	}
 	@Test
-	public void numberinbtw100001to10000000() {
+	public void testnumberinbtw100001to10000000() {
 		Assert.assertEquals("Thirty Five Lakh Thirty Five Thousand Eight Hundred Fifty Four", numberWords.toWords(3535854) ) ;
 	}
 	
